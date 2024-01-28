0d25cec63c60c4399b507fccd9995b7c09115ee3
Law Thai <thaivanluat98@gmail.com>
2024-01-28 20:04:22 +0700

    amend docker build

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -2,3 +2,12 @@
 
 envsubst '$NGINX_ROOT $NGINX_FPM_HOST $NGINX_FPM_PORT' < /etc/nginx/fpm.tmpl > /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
+
+WORKDIR /usr/src/app
+
+# Copy the example environment file
+RUN cp .env.example .env
+
+# Generate the application key
+RUN php artisan key:generate
+
